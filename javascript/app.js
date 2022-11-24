651fe4e81ce9d6ff5cd7243c63dc9842a5f33947
Tolgahan Tolu <tolgahanntolu@gmail.com>
2022-11-24 21:31:33 +0300

    dry principle

--- javascript/app.js
+++ javascript/app.js
@@ -21,74 +21,79 @@ input.addEventListener("focusout", () => {
   inputPlaceholder.classList.add("d-flex");
 });
 
-// featured products list
 window.addEventListener("DOMContentLoaded", async () => {
-  const featuredPresponse = await fetch(featuredProductsData);
-  const featuredPdata = await featuredPresponse.json();
-  featuredPdata.map((item) => {
-    const html = `
-				<div class="product product--featured overflow-hidden position-relative">
-					<small class="text-uppercase fw-bolder">Web'e Özel</small>
-					<img src="${item.img}"
-						alt="product image" class="my-4">
-					<div class="d-flex align-items-center gap-1">
-						<span><i class="fa-solid fa-star"></i>${item.rating}</span>
-						<span>(${item.comment} Yorum)</span>
-					</div>
-					<p class="product__pcode text-uppercase mt-4">${item.code}</p>
-					<h5 class="product__title text-uppercase text-center mt-1">${item.title}</h5>
-					<h1 class="product__price my-2">₺${item.price}</h1>
-					<p class="product__count text-uppercase fw-bolder">yalnızca 2 ürün kaldı</p>
-					<small class="text-uppercase rounded px-3 py-2">
-					${item.samedayshipping ? "bugün kargoda" : ""}	
-					</small>
+  fetchData(featuredProductsData, "list", featuredProducts);
 
-					<div class="product__addtocart w-100 d-flex position-absolute bottom-0 left-0">
-						<button type="button">
-							<i class="fa-solid fa-right-left"></i>
-						</button>
-						<button type="button" class="text-uppercase fw-bolder">Sepete
-							ekle</button>
-					</div>
-				</div>
-  `;
-
-    return featuredProducts.insertAdjacentHTML("afterbegin", html);
-  });
-
-  const bestSellerPResponse = await fetch(bestSellerProductsData);
-  const bestSellerPdata = await bestSellerPResponse.json();
-
-  bestSellerPdata.map((item) => {
-    const html = `
-					<div class="bestSeller__swiper-slide swiper-slide">
-						<div class="product overflow-hidden position-relative">
-							<small class="text-uppercase fw-bolder">Web'e Özel</small>
-							<img src="${item.img}"
-								alt="product image" class="my-4">
-							<div class="d-flex align-items-center gap-1">
-								<span><i class="fa-solid fa-star"></i>${item.rating}</span>
-								<span>(${item.comment} Yorum)</span>
-							</div>
-							<p class="product__pcode text-uppercase mt-4">${item.code}</p>
-							<h5 class="product__title text-uppercase text-center mt-1">${item.title}</h5>
-							<h1 class="product__price my-2">₺${item.price}</h1>
-							<p class="product__count text-uppercase fw-bolder">yalnızca 2 ürün kaldı</p>
-							<small class="text-uppercase rounded px-3 py-2">
-								${item.samedayshipping ? "bugün kargoda" : ""}
-							</small>
-
-							<div class="product__addtocart w-100 d-flex position-absolute bottom-0 left-0">
-								<button type="button">
-									<i class="fa-solid fa-right-left"></i>
-								</button>
-								<button type="button" class="text-uppercase fw-bolder">Sepete
-									ekle</button>
-							</div>
-						</div>
-					</div>
-				`;
-
-    return bestSellerProductsSlider.insertAdjacentHTML("beforeend", html);
-  });
+  fetchData(bestSellerProductsData, "slider", bestSellerProductsSlider);
 });
+
+// functions
+const fetchData = async (url, forWhat, toWhere) => {
+  const data = await (await fetch(url)).json();
+
+  data.map((item) => {
+    createHtml(forWhat, item, toWhere);
+  });
+};
+
+const createHtml = (forWhat, item, toWhere) => {
+  let html = undefined;
+  if (forWhat == "slider") {
+    html = `
+		<div class="bestSeller__swiper-slide swiper-slide">
+			<div class="product overflow-hidden position-relative">
+				<small class="text-uppercase fw-bolder">Web'e Özel</small>
+				<img src="${item.img}"
+					alt="product image" class="my-4">
+				<div class="d-flex align-items-center gap-1">
+					<span><i class="fa-solid fa-star"></i>${item.rating}</span>
+					<span>(${item.comment} Yorum)</span>
+				</div>
+				<p class="product__pcode text-uppercase mt-4">${item.code}</p>
+				<h5 class="product__title text-uppercase text-center mt-1">${item.title}</h5>
+				<h1 class="product__price my-2">₺${item.price}</h1>
+				<p class="product__count text-uppercase fw-bolder">yalnızca 2 ürün kaldı</p>
+				<small class="text-uppercase rounded px-3 py-2">
+					${item.samedayshipping ? "bugün kargoda" : ""}
+				</small>
+
+				<div class="product__addtocart w-100 d-flex position-absolute bottom-0 left-0">
+					<button type="button">
+						<i class="fa-solid fa-right-left"></i>
+					</button>
+					<button type="button" class="text-uppercase fw-bolder">Sepete
+						ekle</button>
+				</div>
+			</div>
+		</div>
+	`;
+  } else {
+    html = `
+		<div class="product product--featured overflow-hidden position-relative">
+			<small class="text-uppercase fw-bolder">Web'e Özel</small>
+			<img src="${item.img}"
+				alt="product image" class="my-4">
+			<div class="d-flex align-items-center gap-1">
+				<span><i class="fa-solid fa-star"></i>${item.rating}</span>
+				<span>(${item.comment} Yorum)</span>
+			</div>
+			<p class="product__pcode text-uppercase mt-4">${item.code}</p>
+			<h5 class="product__title text-uppercase text-center mt-1">${item.title}</h5>
+			<h1 class="product__price my-2">₺${item.price}</h1>
+			<p class="product__count text-uppercase fw-bolder">yalnızca 2 ürün kaldı</p>
+			<small class="text-uppercase rounded px-3 py-2">
+			${item.samedayshipping ? "bugün kargoda" : ""}	
+			</small>
+
+			<div class="product__addtocart w-100 d-flex position-absolute bottom-0 left-0">
+				<button type="button">
+					<i class="fa-solid fa-right-left"></i>
+				</button>
+				<button type="button" class="text-uppercase fw-bolder">Sepete
+					ekle</button>
+			</div>
+		</div>
+  	`;
+  }
+  return toWhere.insertAdjacentHTML("afterbegin", html);
+};
